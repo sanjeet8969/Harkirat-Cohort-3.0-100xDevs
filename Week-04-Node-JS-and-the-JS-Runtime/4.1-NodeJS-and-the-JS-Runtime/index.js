d17d5ab3a8f910e6ddaea53d6b8f94b3b211adf9
Sanjeet Kumar Gupta <sanjeetskg1212@gmail.com>
2024-10-02 07:22:40 +0530

    adding

---
+++ Week-04-Node-JS-and-the-JS-Runtime/4.1-NodeJS-and-the-JS-Runtime/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .name("counter")
+  .description("CLI to do file based tasks")
+  .version("0.8.0");
+
+program
+  .command("count")
+  .description("Count the number of words in a file")
+  .argument("<file>", "file to count")
+  .action((file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let words = 0;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i] === " ") {
+            words++;
+          }
+        }
+
+        console.log(`There are ${words + 1} lines in ${file}`);
+      }
+    });
+  });
+program.parse(process.argv);
